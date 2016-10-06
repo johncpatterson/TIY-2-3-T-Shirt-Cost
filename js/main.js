c9aa00dae5ebe824303dcf831d222866fb13115a
[John Patterson] <pattersonjc1@gmail.com>
2016-10-05 21:04:50 -0400

    moved and styled shopping cart

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
+
 var cartOne = document.getElementById("cart_one");
 var cartTwo = document.getElementById("cart_two");
 var cartThree = document.getElementById("cart_three");
-
 var subtotal = 0;
-
 var blueInCart = false;
 var pinkInCart = false;
 var orangeInCart = false;
 
 
 function updateCart(price) {
+    // tax = Math.abs(price) * .065;
     subtotal = subtotal + price;
-    total = subtotal * .065 + subtotal;
-    document.getElementById("subtotal_box").innerHTML = subtotal;
-    document.getElementById("totalprice_box").innerHTML = total;
+    total = (subtotal * .065) + subtotal;
+    
+    document.getElementById("subtotal_box").innerHTML = subtotal.toFixed(0);
+    // document.getElementById("tax_box").innerHTML = tax.toFixed(2);
+    document.getElementById("totalprice_box").innerHTML = total.toFixed(2);
+    
 
 }
 cartOne.addEventListener("click", function() {
