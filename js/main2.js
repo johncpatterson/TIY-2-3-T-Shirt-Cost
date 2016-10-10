bae6224dc2cb978c119a201a409528f1a3a39b0f
[John Patterson] <pattersonjc1@gmail.com>
2016-10-10 07:27:18 -0400

    added cart animation as well as moment time product was added to the cart and objects to store product info

--- js/main2.js
+++ js/main2.js
@@ -1,67 +1,132 @@
+// variables 
 var cartOne = document.getElementById("cart_one");
 var cartTwo = document.getElementById("cart_two");
 var cartThree = document.getElementById("cart_three");
 var tax = document.getElementById("tax_box");
+var cartToggle = document.getElementById("cart_logo");
+var PlaceHolder = document.getElementById("product_box");
+
 var subtotal = 0;
 var blueInCart = false;
 var pinkInCart = false;
 var orangeInCart = false;
+var cart = false;
+var dateAdded = new Date();
+var text = "";
 
+// empty array to hold cart contents
+var cartContents = [];
 
+// shirt objects to store info about shirts
+var blueShirt = {
+    color: "blue",
+    name: "Save my trees",
+    price: 29,
+    time: dateAdded
+}
+var pinkShirt = {
+    color: "pink",
+    name: "Nature Lover",
+    price: 19,
+    time: dateAdded
+}
+var orangeShirt = {
+    color: "orange",
+    name: "Forrest Walk",
+    price: 39,
+    time: dateAdded
+}
+
+// funtion for updating the price of each item added to the cart and adding the items to the HTML on the page
 function updateCart(price) {
-    // tax = Math.abs(price) * .065;
-    subtotal = subtotal + price;
-    total = (subtotal * .065) + subtotal;
+    // reset innerHTML and what's in text variable
+    text = "";
+    PlaceHolder.innerHTML = "";
+    subtotal = 0;
+    // loop through array and add shirt info and time added to page 
+    cartContents.forEach(function(shirt, index) {
+        subtotal += shirt.price;
+        timeAdded = moment(shirt.time).fromNow();
+        text +=
+            `<div><span class="shirt_name">${shirt.name}</span> 
+            <span class="shirt_color">${shirt.color}</span> 
+            <span class="shirt_price">$${shirt.price}</span>
+            <span class="time">${timeAdded}</span>
+            </div>
+            `;
+    });
 
-    document.getElementById("subtotal_box").innerHTML = subtotal.toFixed(0);  /*round decimal places to 0*/
-    // document.getElementById("tax_box").innerHTML = tax.toFixed(2);
-    document.getElementById("totalprice_box").innerHTML = total.toFixed(2);    /*round decimal places to */
-    return total;
+    PlaceHolder.innerHTML = text;
+    tax = subtotal * .065;
+    total = tax + subtotal;
+
+    document.getElementById("subtotal_box").innerHTML = subtotal.toFixed(0); /*round decimal places to 0*/
+    document.getElementById("tax_box").innerHTML = tax.toFixed(2);
+    document.getElementById("totalprice_box").innerHTML = total.toFixed(2);
+
 
 }
 
-// function bounceCart(bounce) {
-// 	if (blueInCart === true || pinkInCart === true || orangeInCart === true) {
-// 		var a = document.getElementByID('cart')
-//           for (i = 0; i < a.length; i++) {
-//             [i].classList.remove('clicked');
-//         }
-//         document.getElementById('cart').classList.add("clicked");
-// 	}
-// 	else if (blueInCart === false || pinkInCart === false || orangeInCart === false) {
-
-// 	}
-// }
-
-cartOne.addEventListener("click", function() {
-    if (blueInCart === false) {
-        updateCart(29);
-        blueInCart = true;
+// event listener to show and hide cart
+cartToggle.addEventListener("click", function() {
+    if (cart === false) {
+        cart = true;
+        cart_stuff_to_hide.className = "cart";
     } else {
-        updateCart(-29);
-        blueInCart = false;
+        cart = false;
+        cart_stuff_to_hide.className = "hidden";
     }
 
+});
+
+// event listeners to do stuff when clicking on the shopping cart icons to add and remove items from the cart
+cartOne.addEventListener("click", function() {
+    if (blueInCart === false) {
+        blueShirt.time = new Date();
+        cartContents.push(blueShirt);
+        updateCart();
+        blueInCart = true;
+        cart_one.className = "small_pic_5 active";
+    } else {
+        cartContents.pop(blueShirt);
+        updateCart();
+        blueInCart = false;
+        cart_one.className = "small_pic_5";
+    }
+
+    console.log(cartContents);
 });
 
 cartTwo.addEventListener("click", function() {
     if (pinkInCart === false) {
-        updateCart(19);
+        pinkShirt.time = new Date();
+        cartContents.push(pinkShirt);
+        updateCart();
         pinkInCart = true;
+        cart_two.className = "small_pic_5 active";
     } else {
-        updateCart(-19);
+        cartContents.pop(pinkShirt);
+        updateCart();
         pinkInCart = false;
+        cart_two.className = "small_pic_5";
     }
 
+    console.log(cartContents);
 });
 
 cartThree.addEventListener("click", function() {
     if (orangeInCart === false) {
-        updateCart(39);
+        orangeShirt.time = new Date();
+        cartContents.push(orangeShirt);
+        updateCart();
         orangeInCart = true;
+        cart_three.className = "small_pic_5 active";
     } else {
-        updateCart(-39);
+        cartContents.pop(orangeShirt);
+        updateCart();
         orangeInCart = false;
+        cart_three.className = "small_pic_5";
     }
 
+    console.log(cartContents);
 });
